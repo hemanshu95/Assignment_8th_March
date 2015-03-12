8813cce0e1bae44649be0fe59ca696bfe90f63d3
cryptomanic <deepakyadav.iitbhu@gmail.com>
2015-03-13 00:12:44 +0530

    Basic outlook made but need to do algo

--- LPSolver.py
+++ LPSolver.py
@@ -3,3 +3,9 @@ class LPsolver:
         C, B, x, maxPos, minPos = [], [], [], [], []
         for i in c:
             C += [[j for j in i]]
+        x= [0 for j in range(len(a))]    
+        for i in range(len(b)):
+            C[i] += [0 for i in range(len(b))]
+            C[i][len(a)+i] = 1
+        a += [0 for i in range(len(b))]
+        B = [0 for i in range(len(b))]    
