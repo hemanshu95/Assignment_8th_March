82a6dba84f0bff145ff99de46bc7fc623649d0d8
cryptomanic <deepakyadav.iitbhu@gmail.com>
2015-03-13 12:48:24 +0530

    gauss method completed

---
+++ LinearSystemSolver.py
@@ -0,0 +1,25 @@
+class LinearSystemSolver:
+    def Gauss(self,A,b):
+        C = []
+        Ans = []
+        for i in A:
+            C += [[j for j in i]+[b[A.index(i)]]]
+        for i in range(1,len(b)):
+            run = C[i-1][i-1]
+            for j in range(len(b)+1):
+                    C[i-1][j] /= run
+            for j in range(i,len(b)):
+                run = C[j][i-1]
+                for k in range(len(b)+1):
+                    C[j][k] -= run*C[i-1][k]
+        run = C[i][i]
+        for j in range(len(b)+1):
+                    C[i][j] /= run                    
+        C.reverse()            
+        for i in range(len(b)):
+            run = 0
+            for j in range(len(Ans)):
+                run += C[i][len(b)-1-j]*Ans[j]
+            Ans.append(C[i][len(b)]-run)
+        Ans.reverse()    
+        return Ans
