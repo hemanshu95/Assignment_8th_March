c9cd19592136832389e6c964557dcb7632f30f70
Anandita29 <Anandita29@users.noreply.github.com>
2015-03-13 22:25:01 +0530

    Interpolate completed

--- Class Interpolate.py
+++ Class Interpolate.py
@@ -19,6 +19,31 @@ class Interpolate:
         for i in range(n):
             result+=(P.polydiv(w,(-1*A[i],1))[0]*B[i])/P.polyval(A[i],derivative)   
         return(list(result)) 
+        
+    def Newton(self,A,B):                                                   
+       
+        from numpy import array
+        from numpy.polynomial import polynomial as P
+        n=len(A)                                                            
+        mat=[[0.0 for i in range(n)] for j in range(n)]                    
+        for i in range(n):                                                 
+            mat[i][0]=B[i]
+        for i in range(1,n):                                               
+            for j in range(n-i):
+                mat[j][i]=(mat[j+1][i-1]-mat[j][i-1])/(A[j+i]-A[j])
+        res=array((mat[0][0],))                                          
+        for i in range(1,n):
+            prod=(-1*A[0],1)                                               
+                                                                            
+            for j in range(1,i):
+                prod=P.polymul(prod,(-1*A[j],1))                              
+            res=P.polyadd(res,array(prod)*mat[0][i])                  
+        return (list(res))                                               
+
+apx=Interpolate()                                                          
+for method in ["newton","lagrange"]:
+    sol=apx.solve([1,2,3],[0,-1,0],method)
+    print(sol)
                
     
 
