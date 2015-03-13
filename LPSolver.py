01db15d5f69c1afe90c3930ea7d59e8bc47287c9
cryptomanic <deepakyadav.iitbhu@gmail.com>
2015-03-13 07:57:21 +0530

    almost half done

--- LPSolver.py
+++ LPSolver.py
@@ -9,3 +9,33 @@ class LPsolver:
             C[i][len(a)+i] = 1
         a += [0 for i in range(len(b))]
         B = [0 for i in range(len(b))]    
+        while True:
+            maxPos, minPos = [], []
+            for i in range(len(a)):
+                sum = 0
+                for j in range(len(b)):
+                    sum += C[j][i]*B[j]
+                maxPos += [a[i]-sum]
+            counter = 0
+            for i in maxPos:
+                if i <= 0:
+                     counter += 1
+            if counter == len(a):
+                 break
+            index1 = maxPos.index(max(maxPos))    
+            for i in range(len(b)):
+                if C[i][index1] != 0:
+                     minPos += [b[i]/C[i][index1]]
+                elif b[i] > 0:
+                    minPos += ["infinity"]
+                else:
+                    minPos += ["-infinity"]
+            counter = 0
+            for i in minPos:
+                if i != "infinity":
+                    if i == "-infinity":
+                        counter += 1
+                    elif i <= 0:
+                        counter += 1
+            if counter == len(b):
+                return "problem is unbounded"
