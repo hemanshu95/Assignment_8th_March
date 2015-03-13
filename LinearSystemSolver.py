f31fd1349c2c83efae0762579764db7f173d5d44
cryptomanic <deepakyadav.iitbhu@gmail.com>
2015-03-13 13:55:28 +0530

    algorithm of gauss jordan completed

--- LinearSystemSolver.py
+++ LinearSystemSolver.py
@@ -23,3 +23,20 @@ class LinearSystemSolver:
             Ans.append(C[i][len(b)]-run)
         Ans.reverse()    
         return Ans
+    def GaussJordan(self,A,b):
+        C = []
+        Ans = []
+        for i in A:
+            C += [[j for j in i]+[b[A.index(i)]]]
+        for i in range(1,len(b)+1):
+            run = C[i-1][i-1]
+            for j in range(len(b)+1):
+                    C[i-1][j] /= run
+            for j in range(len(b)):
+                if j != (i-1):
+                   run = C[j][i-1]
+                   for k in range(len(b)+1):
+                       C[j][k] -= run*C[i-1][k]                
+        for i in range(len(b)):
+            Ans.append(C[i][len(b)])
+        return Ans
